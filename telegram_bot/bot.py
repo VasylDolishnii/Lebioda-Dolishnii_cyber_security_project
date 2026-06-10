9bacfe88720bc0870e3623af69a15d5a5f131f1c
Vasyl Dolishnii <dolishniivasyl@gmail.com>
2026-06-10 23:23:31 +0200

    Add Telegram bot

--- telegram_bot/bot.py
+++ telegram_bot/bot.py
@@ -1,10 +1,10 @@
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
 
-TOKEN = "8863109789:AAGoXUD5iMXjWeGueRjYEW-EB2Irl2NLTX0"
+TOKEN = "XXX"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Hello it`s working Mr. Zigman 🚀")
+    await update.message.reply_text("Hello it`s working 🚀")
 
 app = Application.builder().token(TOKEN).build()
 
